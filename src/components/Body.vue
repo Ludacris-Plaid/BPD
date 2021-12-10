fc8973e04b48e7b2241340b9502a3f4be4fe07d9
Ludacris-Plaid <indicationsmedia@protonmail.com>
2021-12-09 22:34:57 -0700

    first commit

---
+++ src/components/Body.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="body-main">
+    <div id="body-main-1">
+      <div></div>
+      <div id="center-2">
+        <iframe
+          src="https://giphy.com/embed/vJHK6Js1M9FOE"
+          width="261"
+          height="587"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+        <p><a href="https://giphy.com/gifs/trippy-pills-vJHK6Js1M9FOE"></a></p>
+      </div>
+      <div></div>
+    </div>
+    <div id="body-main-2"></div>
+    <div id="body-main-3">
+      <div></div>
+      <div id="center">
+        <iframe
+          src="https://giphy.com/embed/vJHK6Js1M9FOE"
+          width="261"
+          height="587"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+        <p><a href="https://giphy.com/gifs/trippy-pills-vJHK6Js1M9FOE"></a></p>
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.body-main {
+  display: grid;
+  /* width: 100%; */
+  height: 100vh;
+  grid-template-columns: 1fr 3fr 1fr;
+  border-radius: 10px;
+}
+#body-main-1 {
+  background-color: #91a1ba;
+  border-radius: 10px;
+  /* align-items: center; */
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+}
+#body-main-2 {
+  background-color: #91a1ba75;
+  border-radius: 10px;
+}
+#body-main-3 {
+  background-color: #91a1ba;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  /* align-items: center; */
+}
+</style>
