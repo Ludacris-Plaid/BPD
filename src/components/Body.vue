08effcfc98cd0440113a86fcdca0b9fb3c929c82
Ludacris-Plaid <indicationsmedia@protonmail.com>
2021-12-13 21:38:17 -0700

    added clownworld videos

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -18,14 +18,112 @@
     <div id="body-main-2">
       <div id="main-row-1">
         <h1>THIS PAGE IS UNDER CONSTRUCTION</h1>
-        <P>Check Back soon...</P>
-        <img src ="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.uq5HvfHzAPbpdRFMJuVS6AHaLL%26pid%3DApi&f=1">
+        <p>---------------</p>
+        <p2
+          >Here is a perfect example of the clownworld. Watch as a Canadian MP
+          asks a dozen of our "elected officials" (including the finance
+          minister) where 7 BILLION dollars is coming from needed to pay for the
+          newest round of insane COVID spending.</p2
+        >
+        <br />
+        <iframe
+          width="760"
+          height="430"
+          src="https://www.youtube.com/embed/mapxta3nKcA"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <br />
+        <p2
+          >Not one of these baffoons making 200k a year is even brave enough to
+          speak up.</p2
+        >
+        <br />
+        <p2> CLOWNWORLD AT ITS FINEST.</p2>
+        <br />
+        <p>---------------</p>
+        <br />
+        <p2
+          >In another stunning example of the clownworld we see an Ontario MPP
+          mention a massive increase in still births being reported by doctors
+          in his riding. All while being heckled by his peers. We then get the
+          amazing clownworld response from a Queen of Clowns Christine
+          Elliott.</p2
+        >
+        <br />
+        <iframe
+          width="760"
+          height="430"
+          src="https://www.youtube.com/embed/dcsQmJ9q7eU"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <br />
+        <p2>
+          I hope her Pfizer money will cover some plastic surgery for that ugly
+          mug.</p2
+        >
+        <br />
+        <p2>PEAK CLOWNWORLD</p2>
+        <br />
+        <p>---------------</p>
+        <br />
+        <p2
+          >From the Nazi's down under here is an Australian Premier going off on
+          "Anti-Vaxxers'. Like it's ilegal or some shit. Yeah I'm anti-this-vaxx
+          you degenerate little puke. Imagine shilling for a fauxine THAT
+          DOESN'T EVEN WORK.</p2
+        >
+        <br />
+        <iframe
+          width="760"
+          height="430"
+          src="https://www.youtube.com/embed/YOw8HunNVQM"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <br />
+        <p2>
+          Wonder how much this prick is geting paid by Pfizer. Hopefully it's
+          enough to buy the buds this snapshow so obviously needs.</p2
+        >
+        <br />
+        <p2>CLOWNWORLD AUSTRALIAN STYLE</p2>
+        <br />
+        <p>---------------</p>
+        <br />
+        <p2
+          >Need some help navigating the CLOWNWORLD? Do you have questions about
+          how we got here? Adam Curtis (Century of the Self) breaks down his
+          theory on society that he calls hypernormalization. All of his work is
+          stunning and genius but this could be the most important documentary you'll ever see.</p2
+        >
+        <br />
+        <iframe
+          width="760"
+          height="430"
+          src="https://www.youtube.com/embed/thLgkQBFTPw"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <br />
+        <p2>But this is a must see if you need answers.</p2>
         <br>
         <a href="https://blackpilldiaries.substack.com"
-          >Meanwhile check out my substack!</a>
+          >Meanwhile check out my substack!</a
+        >
       </div>
       <div id="main-row-2"></div>
       <div id="main-row-3">
+        <!-- <P>Check Back soon...</P> -->
         <p2>&#169; 2021 Indications Media</p2>
       </div>
     </div>
@@ -48,9 +146,7 @@
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style scoped>
@@ -74,7 +170,7 @@ export default {
   font-family: "Permanent Marker", cursive;
   text-align: center;
   display: grid;
-  grid-template-rows: 2fr 2fr .2 fr;
+  grid-template-rows: 2fr 2fr 0.2 fr;
 }
 #body-main-3 {
   background-color: #91a1ba;
@@ -84,24 +180,20 @@ export default {
   /* align-items: center; */
 }
 p2 {
-  font-family:"Bebas Neue", cursive;
-;
+  font-family: "Bebas Neue", cursive;
 }
-#main-row-1{
+#main-row-1 {
   align-items: center;
 }
-@media screen and (max-width :500px){
+@media screen and (max-width: 500px) {
   .body-main {
     display: inherit;
-
   }
   img {
-  width: 100%;
+    width: 100%;
+  }
+  #body-main-3 {
+    display: none;
+  }
 }
-#body-main-3 {
-  display: none;
-}
-}
-
-
 </style>
