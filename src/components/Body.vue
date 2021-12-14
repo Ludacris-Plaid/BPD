7094515cdceb011a614f6b4a4ec8178d2241d587
Ludacris-Plaid <indicationsmedia@protonmail.com>
2021-12-13 17:20:47 -0700

    fixed mobile

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -19,10 +19,10 @@
       <div id="main-row-1">
         <h1>THIS PAGE IS UNDER CONSTRUCTION</h1>
         <P>Check Back soon...</P>
-        <a href="https://blackpilldiaries.substack.com"
-          >Meanwhile check out my substack!</a>
         <img src ="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.uq5HvfHzAPbpdRFMJuVS6AHaLL%26pid%3DApi&f=1">
         <br>
+        <a href="https://blackpilldiaries.substack.com"
+          >Meanwhile check out my substack!</a>
       </div>
       <div id="main-row-2"></div>
       <div id="main-row-3">
